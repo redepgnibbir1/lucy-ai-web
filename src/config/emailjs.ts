8599032352f8752f6445f4c0f96dbb3723e0b3e0
redepgnibbir1 <peder.ribbing@lucyanalytics.com>
2025-04-29 11:20:31 +0200

    Update emailjs.ts

--- src/config/emailjs.ts
+++ src/config/emailjs.ts
@@ -1,5 +1,5 @@
 export const EMAILJS_CONFIG = {
-  serviceId: 'service_v7bcsd8', // Replace with your EmailJS service ID
+  serviceId: 'service_z0cv4gw', // Replace with your EmailJS service ID
   templateId: 'template_ffo0qkm', // Replace with your EmailJS template ID
   publicKey: 'BDWXvtbH1CZ-0N4rG', // Replace with your EmailJS public key
   toEmail: 'superdevninja0304@gmail.com'
